ffa054a7517497c11f1a5461ff5cf7f4fbdbba78
Artem Golubev <artem.golubev@gmail.com>
2015-11-20 11:45:11 -0800

    Initial commit

---
+++ install_vagrant.sh
@@ -0,0 +1,5 @@
+sudo apt-get install libc6 libcurl3 libdevmapper1.02.1 libgcc1 libgl1-mesa-glx libpng12-0 libpython2.7 libqt4-opengl libqtcore4 libqtgui4 libqt4-opengl libssl1.0.0 libstdc++6 libvpx1 libx11-6 libxcursor1 libxext6 libxinerama1 libxml2 libxmu6 libxt6 zlib1g psmisc adduser dkms binutils
+sudo apt-get install libasound2 libsdl1.2debian libasound2-data libcaca0 libpulse0 libasyncns0 libsndfile1 libflac8 libogg0 libvorbis0a libvorbisenc2
+sudo dpkg -i virtualbox-5.0_5.0.10-104061~Ubuntu~trusty_amd64.deb
+sudo dpkg -i vagrant_1.7.4_x86_64.deb
+
